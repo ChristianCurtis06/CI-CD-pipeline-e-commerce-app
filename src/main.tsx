dfcb4feeee9490527cd7f8e70aa99adddc37673e
ChristianCurtis06 <ctcurtis10@gmail.com>
2025-04-04 10:00:25 -0500

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import App from './App.tsx'
+import App from './App'
 
 const queryClient = new QueryClient();
 const rootElement: any = document.getElementById('root');
